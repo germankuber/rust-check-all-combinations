2dea0c349a3f9b947e3c879fef9ccd291d860b75
German Kuber <german.kuber@outlook.com>
2022-09-01 23:44:19 -0300

    First commit

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ fn main() {
         'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
     ];
     do_all_work(0, &mut result);
-    // let result = generate_new_result("00000Y000C00000Y", "A");
-
-    // print!("{}", result);
 }
 
 fn do_all_work(result_char_count: usize, result: &mut [char; 16]) -> Result<(), ()> {
@@ -28,42 +25,6 @@ fn do_all_work(result_char_count: usize, result: &mut [char; 16]) -> Result<(), 
     return Ok(());
 }
 
-// fn generate_new_result<'a>(word: &'a mut [char], list_of_characters: &[char]) -> &'a [char] {
-//     // let mut position_of_all_character = 0;
-
-//     // while true {
-//     //     let char_to_use = list_of_characters[position_of_all_character];
-
-//     //     if position_of_all_character == 36 {
-//     //         position_of_all_character = 0;
-//     //     }
-//     //     position_of_all_character += 1;
-//     // }
-
-//     for char in word.iter() {
-//         fn_algo(char)
-//     }
-
-//     let length = word.len();
-
-//     let word_to_use = &word[0..(length - 1)];
-
-//     let ll = word_to_use.len();
-
-//     return word;
-// }
-
-// A A A A
-
-//RESULT = """
-// A 0 , RESULT = "0000"
-// A 1 , RESULT = "0000"
-// 0 2 , RESULT = "0000"
-// 0 3 , RESULT = "0000"
-// 0 3 , RESULT = "0001"
-// 0 3 , RESULT = "0002"
-
-//A 0 ""
 //RESULT = "AAA"
 //do_all_work(0, "A")
 //  do_all_work(1, "A")
